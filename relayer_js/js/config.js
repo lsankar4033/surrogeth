0a9017d3d18d6c5a12989ceb31b55a6047a2aed5
lsankar4033 <lsankar4033@gmail.com>
2019-08-15 14:07:13 -0700

    Add '/fee method

--- relayer_js/js/config.js
+++ relayer_js/js/config.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 
-module.exports = {
-  KOVAN_RPC_URL: process.env.KOVAN_RPC_URL,
-  MAINNET_RPC_URL: process.env.MAINNET_RPC_URL,
-  PRIVATE_KEY: process.env.PRIVATE_KEY,
+const envVars = [
+  'KOVAN_RPC_URL',
+  'MAINNET_RPC_URL',
+  'PRIVATE_KEY',
+  'MIN_TX_PROFIT' // minimum profit per tx (in Wei) to consider submitting it
+];
+
+let e = {}
+for (const envVar of envVars) {
+  e[envVar] = process.env[envVar]
 }
+
+module.exports = e
--- relayer_js/js/index.js
+++ relayer_js/js/index.js
@@ -6,34 +6,40 @@ const Web3 = require('web3');
 const Accounts = require('web3-eth-accounts');
 const accounts = new Accounts();
 
-const { KOVAN_RPC_URL, PRIVATE_KEY } = require('./config');
+const { createForkedWeb3, simulateTx } = require('./ethereum');
+const { KOVAN_RPC_URL, PRIVATE_KEY, MIN_TX_PROFIT } = require('./config');
 const ADDRESS = accounts.privateKeyToAccount(PRIVATE_KEY).address
 
-// TODO: Better logging
 const app = express();
-
-/**
- * Create a forked version of web3 using the provided rpcUrl
- */
-const createForkedWeb3 = (rpcUrl) => {
-  return new Web3(
-    ganache.provider({
-      'fork': rpcUrl
-    })
-  );
-};
-
-/**
- * Simulate running a tx with the specified web3 instance and return the resulting web3 instance
- */
-const simulateTx = async (web3, to, data, value) => {
-
-  console.log('hi');
-
-}
+const web3 = new Web3(KOVAN_RPC_URL);
 
 app.get('/address', (req, res) => {
   res.json({ address: ADDRESS });
+});
+
+app.get('/fee', [
+  check('to').isHexadecimal(),
+  check('data').isHexadecimal(),
+  check('value').isInt()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+  }
+
+  const { to, data, value } = req.query;
+
+  const gasPrice = parseInt(await web3.eth.getGasPrice());
+  const gasEstimate = await web3.eth.estimateGas({
+    to,
+    data,
+    value,
+    from: ADDRESS
+  });
+
+  const cost = gasPrice * gasEstimate;
+
+  res.json({ fee: cost + MIN_TX_PROFIT });
 });
 
 app.post('/submit_tx', [
