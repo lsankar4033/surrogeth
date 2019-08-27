4d135f1e2f938c32c056d9d254749a186008fcd3
lsankar4033 <lsankar4033@gmail.com>
2019-08-27 16:38:08 -0700

    Run prettier against everything

--- relayer/js/config.js
+++ relayer/js/config.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const envVars = [
-  'KOVAN_RPC_URL',
-  'MAINNET_RPC_URL',
-  'RELAYER_PRIVATE_KEY',
-  'RELAYER_MIN_TX_PROFIT',       // minimum profit per tx (in Wei) to consider submitting it
+  "KOVAN_RPC_URL",
+  "MAINNET_RPC_URL",
+  "RELAYER_PRIVATE_KEY",
+  "RELAYER_MIN_TX_PROFIT" // minimum profit per tx (in Wei) to consider submitting it
 ];
 
-let e = {}
+let e = {};
 for (const envVar of envVars) {
-  e[envVar] = process.env[envVar]
+  e[envVar] = process.env[envVar];
 }
 
-module.exports = e
+module.exports = e;
--- relayer/js/utils.js
+++ relayer/js/utils.js
@@ -1,18 +1,16 @@
 hexStrRE = /^0x[0-9A-Fa-f]+$/;
 
-const isHexStr = (s) => {
-  return (s.length % 2) == 0 &&
-    hexStrRE.test(s);
-}
+const isHexStr = s => {
+  return s.length % 2 == 0 && hexStrRE.test(s);
+};
 
-const isAddressStr = (s) => {
-  return s.length == 42 &&
-    hexStrRE.test(s);
-}
+const isAddressStr = s => {
+  return s.length == 42 && hexStrRE.test(s);
+};
 
-const isNetworkStr = (s) => {
-  return s === 'MAINNET' || s === 'KOVAN';
-}
+const isNetworkStr = s => {
+  return s === "MAINNET" || s === "KOVAN";
+};
 
 module.exports = {
   isHexStr,
