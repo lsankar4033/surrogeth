d9a6f9ce6357bc384e2cbc6866cd1dd6c11700f9
lsankar4033 <lsankar4033@gmail.com>
2019-09-10 13:17:29 -0700

    registry -> burnRegistry

---
+++ burnRegistry/truffle-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+      gas: 6721975 // The default is 4712388 and that could get our way if we do load testing for batchRelayCall
+    }
+  },
+
+  compilers: {
+    solc: {
+      version: "0.5.10"
+    }
+  },
+
+  plugins: ["truffle-watch"]
+};
