f7d8f02a5a8b7351afffb2b6e89ef8b3a8fa7da2
lsankar4033 <lsankar4033@gmail.com>
2019-09-19 19:14:47 -0700

    Change exports to return SurrogethClient

--- client/src/client.js
+++ client/src/client.js
@@ -1,10 +1,20 @@
 const ethers = require("ethers");
 
-const getProvider = web3 => {
-  const provider = new ethers.providers.Web3Provider(web3.currentProvider);
-  return provider;
+// TODO: populate once I deploy
+const DEFAULT_REGISTRY_ADDRESSES = {
+  KOVAN: "0x0",
+  MAINNET: "0x0"
 };
 
+class SurrogethClient {
+  constructor(web3, registryAddresses = DEFAULT_REGISTRY_ADDRESSES) {
+    this.provider = new ethers.providers.Web3Provider(web3.currentProvider);
+    this.registryAddresses = registryAddresses;
+  }
+
+  // TODO: getRelayer, submitTx methods
+}
+
 module.exports = {
-  getProvider
+  SurrogethClient
 };
