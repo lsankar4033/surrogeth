d2601a63baa8748eccded60f35ee1aaea630d903
Koh Wei Jie <contact@kohweijie.com>
2019-10-27 07:32:48 +0800

    added Access-Control-Allow-Origin middleware

--- surrogethd/js/app.js
+++ surrogethd/js/app.js
@@ -30,8 +30,19 @@ const lock = new AsyncLock();
 const nonceKey = `nonce_${relayerAccount.address}`;
 
 const app = express();
+
+// enable CORS
 app.use(cors());
 app.options("*", cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.get("/address", (req, res) => {
