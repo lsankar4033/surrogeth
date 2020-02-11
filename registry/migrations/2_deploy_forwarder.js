0deddfdc59ea82dad4ba1bc382ca5e94e473bcf8
lsankar4033 <lsankar4033@gmail.com>
2020-02-11 12:31:50 -0800

    Remove unused migrations and fix used ones

--- registry/migrations/2_deploy_forwarder.js
+++ registry/migrations/2_deploy_forwarder.js
@@ -1,14 +1,10 @@
 const Forwarder = artifacts.require("Forwarder");
 const TestApplication = artifacts.require("TestApplication");
 
-// 1% burn fraction
-const burnNum = 1;
-const burnDenom = 100;
-
 const testFee = 20;
 
 module.exports = (deployer, network) => {
-  deployer.deploy(Forwarder, burnNum, burnDenom);
+  deployer.deploy(Forwarder);
 
   if (network === "development") {
     deployer.deploy(TestApplication, testFee);
--- registry/migrations/4_deploy_erc20_forwarder.js
+++
@@ -1,9 +0,0 @@
-const ERC20Forwarder = artifacts.require("ERC20Forwarder");
-
-// 1% burn fraction
-const burnNum = 1;
-const burnDenom = 100;
-
-module.exports = deployer => {
-  deployer.deploy(ERC20Forwarder, burnNum, burnDenom);
-};
